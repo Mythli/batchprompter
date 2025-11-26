1d7763b523408db20e4bcc8f8496117fe2991810
Tobias <github@mythli.net>
2025-11-26 23:31:31 +0300

    fix: Update image aspect ratio and script permissions

--- examples/10-image/run_2_software.sh
+++ examples/10-image/run_2_software.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 npx tsx src/index.ts generate examples/10-image/data.csv examples/10-image/prompt-2-butlerapp-software \
   --output-1 "out/10-image/2-software/{{id}}.png" \
-  --aspect-ratio-1 "3:2" \
+  --aspect-ratio-1 "1:1" \
   --model "google/gemini-3-pro-image-preview"
