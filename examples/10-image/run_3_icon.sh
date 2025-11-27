d08340cc732b26ab08903f8132816512f03434b9
Tobias <github@mythli.net>
2025-11-28 02:25:12 +0300

    fix: Update output filename for generated icons

--- examples/10-image/run_3_icon.sh
+++ examples/10-image/run_3_icon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 npx tsx src/index.ts generate examples/10-image/data.csv examples/10-image/prompt-4-industry-icon \
-  --output "out/10-image/{{industry}}/{{industry}}-icon.png" \
+  --output "out/10-image/{{industry}}/01_MenuBarIcon.png" \
   --aspect-ratio "1:1" \
   --model "google/gemini-3-pro-image-preview"
