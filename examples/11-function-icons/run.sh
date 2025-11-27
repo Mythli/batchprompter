4c7b696700da82c4bfb2a0563f78baf0333d0207
Tobias <github@mythli.net>
2025-11-27 12:34:47 +0300

    feat: add new example for function icons

---
+++ examples/11-function-icons/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+npx tsx src/index.ts generate examples/11-function-icons/data-test.csv examples/11-function-icons/prompt \
+  --output "out/11-function-icons/{{name}}.png" \
+  --aspect-ratio "1:1" \
+  --model "google/gemini-3-pro-image-preview"
+  --verify-command " pngquant 2 --nofs"
