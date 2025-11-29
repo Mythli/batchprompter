77359f6581a73f238f0fc100e05a44d239aa077e
Tobias <github@mythli.net>
2025-11-29 20:03:05 +0300

    chore: Update image generation run script to reflect new examples

--- examples/10-image/run.sh
+++ examples/10-image/run.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+rm -rf out/10-image
+rm -rf out/10-image-candidates
+
 # Master script to run all image generation tasks sequentially
 # Assumes execution from project root
 
-echo "Running 1-real..."
-./examples/10-image/run_1_real.sh
+#echo "Running 1-real..."
+#./examples/10-image/run-prompt-2-teaser.sh
+#./examples/10-image/run-prompt-3-teaser2.sh
 
-#echo "Running 1b-json-gen..."
-#./examples/10-image/run_1b_json_gen.sh
-
-#echo "Running 2-software..."
-#./examples/10-image/run_2_software.sh
-#
+##echo "Running 1b-json-gen..."
+./examples/10-image/run-prompt-4-booking-form-json-data.sh
+###
+###echo "Running 2-software..."
+./examples/10-image/run-prompt-4-booking-form.sh
+#./examples/10-image/run-prompt-6-dashboard.sh
+###
 #echo "Running 3-icon..."
-#./examples/10-image/run_3_icon.sh
+./examples/10-image/run-10-icon.sh
 
 echo "All done."
