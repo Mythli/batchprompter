cf8e4a3d15609f8ade3923d998a6dfbb3db28903
Tobias <github@mythli.net>
2025-12-07 16:17:40 +0300

    fix

--- examples/13-industry-search/run.sh
+++ examples/13-industry-search/run.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 npx tsx src/index.ts generate examples/13-industry-search/test.csv \
-  "" \
-  "" \
-  --web-query-prompt-1 "Suche nach {{industry}} Firmen" \
-  --web-select-prompt-1 "Versuche echte und eindeutige Firmen-Links aus {{industry}} zu identifizieren" \
-  --web-search-limit-1 5 \
-  --web-search-explode-1 \
-  --website-agent-url-2 "{{link}}" \
-  --website-agent-schema-2 examples/13-industry-search/schemas/contact.json \
-  --website-agent-export-2
+  --web-query-prompt "Suche nach {{industry}} Firmen also Seminaranbieter die Kommunen und grundsätzlich der Regierung Seminare anbieten" \
+  --web-select-prompt "Versuche echte und eindeutige Firmen-Links aus {{industry}} zu identifizieren" \
+  --web-search-limit 5 \
+  --web-search-explode \
+  --website-agent-url "{{webSearch.link}}" \
+  --website-agent-schema examples/13-industry-search/schemas/contact.json \
+  --website-agent-export
