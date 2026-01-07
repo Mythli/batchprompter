89ba693f70a189d716e776da9b8dc0a97bf6cf84
Tobias <github@mythli.net>
2026-01-07 15:29:14 +0300

    some cleanup

--- packages/batchprompt/src/config/ConfigResolver.ts
+++
@@ -1,3 +0,0 @@
-// This file is deprecated and replaced by resolveConfig.ts
-// All logic has been moved to the unified resolveConfig() function.
-export {};
--- packages/batchprompt/src/config/runtimeMapper.ts
+++
@@ -1,4 +0,0 @@
-// This file is deprecated and no longer needed.
-// The resolveConfig() function now directly produces RuntimeConfig
-// with the correct field names, eliminating the need for mapping.
-export {};
